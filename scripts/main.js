d24d7517df5dab35d23a5a98232df4004fc8f9fc
serchonodera117 <sergio.lobo117@gmail.com>
2023-03-28 12:48:53 -0600

    myYTRequest project added

--- scripts/main.js
+++ scripts/main.js
@@ -29,6 +29,10 @@ let myApp = {
                     name:"GPS application",
                     p_img:"images/projets/Kotlin-gps-app/01.png",
                     technology:"images/projets/Icon-tech/kotlin.png",
+                },{
+                  name: "myYTRequest",
+                  p_img:"images/projets/myYTR_cshar/ytr_0.png",
+                  technology:"images/projets/Icon-tech/sechar.png",
                 },
                 {
                     name: "Sales page",
@@ -253,6 +257,51 @@ let myApp = {
                     </a>
                     </div>
                     `,
+                },
+                {
+                  title: 'myYTRequest',
+                  slide_indicator: `
+                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                  `,
+                  images: `
+                  <div class="carousel-item active">
+                      <img src="images/projets/myYTR_cshar/ytr_0.png" class="d-block w-100 image-carousel" alt="...">
+                  </div>
+                  <div class="carousel-item active">
+                    <img src="images/projets/myYTR_cshar/ytr_1.png" class="d-block w-100 image-carousel" alt="...">
+                  </div>
+                  <div class="carousel-item active">
+                    <img src="images/projets/myYTR_cshar/ytr_2.png" class="d-block w-100 image-carousel" alt="...">
+                  </div>
+                  <div class="carousel-item active">
+                    <img src="images/projets/myYTR_cshar/ytr_3.png" class="d-block w-100 image-carousel" alt="...">
+                  </div>
+                  <div class="carousel-item active">
+                    <img src="images/projets/myYTR_cshar/ytr_4.png" class="d-block w-100 image-carousel" alt="...">
+                  </div>
+                  <div class="carousel-item active">
+                    <img src="images/projets/myYTR_cshar/ytr_5.png" class="d-block w-100 image-carousel" alt="...">
+                  </div>
+                  `,
+
+                  description: `
+                  <p><b>Description:</b>A desktop app for windows<br><br>
+                  this is an app designed and created for download files .mp4 and mp3 for Yutube.<br><br>
+                  features: is responsive<br><br>
+                  </p>
+                  <div id="content-buton-project">
+                  <a class="button-go" onclick="open('https://github.com/serchonodera117/myYTRequesr','Project','height=700,width=700')">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                  </svg>
+                  Github
+                  </a>
+                  </div>
+                  `
                 },
                 {
                     title: 'Sales page',
